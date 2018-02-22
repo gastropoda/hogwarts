4cbf8fe45db6998960aba48d6f2420400c463ab6
Artem Baguinski <abaguinski@depraktijkindex.nl>
2018-02-22 07:09:12 +0100

    split update task into subtasks

--- lib/hog/cli/main.rb
+++ lib/hog/cli/main.rb
@@ -7,6 +7,16 @@ module Hog
       def update
         Hog::Update.new.call
       end
+
+      desc "from_readline", "update files from readline"
+      def from_readline
+        Hog::Update.new.from_readline
+      end
+
+      desc "highlights", "update highlight files"
+      def highlights
+        Hog::Update.new.highlights
+      end
     end
   end
 end
--- lib/hog/update.rb
+++ lib/hog/update.rb
@@ -1,10 +1,18 @@
 module Hog
   class Update
     def call
+      from_readline
+      highlights
+    end
+
+    def from_readline
       Hog.input_languages.each do |language|
         puts language
         Hog::Update::Language.new(language: language).call
       end
+    end
+
+    def highlights
       Hog.highlight_files.each do |path|
         puts path
         Hog::Update::Highlights.new(path: path).call
