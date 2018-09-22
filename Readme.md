f8846ab9ffabb0f5cefec17861a524e19ce2b884
Artem Baguinski <abaguinski@gmail.com>
2018-09-22 17:54:23 +0200

    alles

--- Readme.md
+++ Readme.md
@@ -4,4 +4,8 @@ The name: because I read Harry Potter at the moment.
 
 At the moment this converts a readlang export into a cleaned up tab separated table for import into quizlet.
 
+Copy to clipboard:
+
+  xclip -selection clipboard -i ...
+
 [License](./LICENSE): I don't care (public domain).
--- lib/hog/update/highlights.rb
+++ lib/hog/update/highlights.rb
@@ -10,6 +10,7 @@ module Hog
       def call
         require "csv"
         puts " => #{out_path}"
+        chapters
         File.open(path, "r") do |in_io|
           skip_header(in_io)
           CSV.open(out_path, "w", col_sep: "\t") do |out_csv|
@@ -30,19 +31,52 @@ module Hog
           line = line.chomp
           line = line.gsub(/^"|"$/, "")
           if line.empty?
-            out_csv << row
+            process_row(*row).each do |out_row|
+              out_csv << out_row
+            end
             row = []
           else
             row << line
           end
         end
         unless row.empty?
-          out_csv << row
+          process_row(*row).each do |out_row|
+            out_csv << out_row
+          end
         end
+      end
+
+      def process_row(highlight, chapter, *rest)
+        chapter = extract_chapter_name(chapter)
+        clauses = chapters[chapter]
+        raise "Unknown chapter '#{chapter}'" if clauses.nil?
+        clauses
+          .find_all { |clause| clause[/\b#{highlight}\b/] }
+          .map { |clause| [highlight, clause] }
+      end
+
+      def extract_chapter_name chapter
+        chapter.match(/\(Chapter:(.*)\)/)[1]
       end
 
       def out_path
         @out_path ||= File.join(Hog.config.output_path, File.basename(path))
+      end
+
+      def chapters
+        @chapters ||= JSON.parse(canon_json)
+      end
+
+      def canon_json
+        File.read(canon_json_path)
+      end
+
+      def canon_json_path
+        File.join(Hog.config.canon_path, "#{book_title}.json")
+      end
+
+      def book_title
+        File.basename(path, ".txt").sub(/^Share. /, "")
       end
     end
   end
